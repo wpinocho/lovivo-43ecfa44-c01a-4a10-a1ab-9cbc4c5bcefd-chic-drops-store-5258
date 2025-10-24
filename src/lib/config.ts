0ba01e2903582fb656fdd39148005f8f324ea9b0
wpinocho <julian.ruiz.loza@gmail.com>
2025-10-24 08:59:36 -0600

    chore: update store ID to 43ecfa44-c01a-4a10-a1ab-9cbc4c5bcefd

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
-export const STORE_ID = '3e38e69b-4a39-4eac-b6b4-883a4bf4a629'
+export const STORE_ID = '43ecfa44-c01a-4a10-a1ab-9cbc4c5bcefd'
 export const STRIPE_PUBLISHABLE_KEY = 'pk_live_51Rk9SNP66p9BDoW4lgxDnuqvNZYEaL400FCX0GStvuaslkEMBZNRqln5M5bDcBSBmFmR7hlY0pDcA4u4VIi8CRIg00KkqRzlVs'
 export const LOVIVO_STRIPE_ACCOUNT_ID = 'acct_1Rk9SNP66p9BDoW4'
